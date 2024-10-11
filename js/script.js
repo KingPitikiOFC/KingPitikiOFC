4ab38caa485e2e321cb7601faa8944c57ebbacfe
KingPitikiOFC <camiyt2909@gmail.com>
2024-10-11 06:15:41 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-// Reproducir música de fondo al cargar la página
-window.onload = function() {
+// Reproducir música de fondo al hacer clic en el botón
+document.getElementById('playButton').onclick = function() {
     const musica = document.getElementById('musica');
     musica.play().catch((error) => {
         console.log('Error al reproducir la música: ' + error);
